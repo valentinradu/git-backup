7b711d75ad0b05fe12d5759355abda57273658ea
Valentin Radu <radu.v.valentin@gmail.com>
2017-09-08 15:53:07 +0300

    [Backup Commit]

--- backup.py
+++ backup.py
@@ -2,21 +2,38 @@
 
 import git
 import sys
+import os
 
-
+def notify(title, text):
+    os.system("""
+              osascript -e 'display notification "{}" with title "{}"'
+              """.format(text, title))
+              
 if __name__ == '__main__':
     
     paths = sys.argv[1:]
     
+    fails = []
     for path in paths:
+        try:
+            repo = git.Repo(path)
+            
+            if repo.bare:
+                continue
+            
+            if repo.is_dirty():
+                repo.git.add(u=True)
+                repo.git.commit('-m', '[Backup Commit]', author='radu.v.valentin@gmail.com')
+                repo.git.push()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            fails.append(path)
         
-        repo = git.Repo(path)
+    if len(fails) > 0:
+        notify('Git auto push', '{} failed.'.format(", ".join(fails)))
         
-        if repo.bare:
-            continue
         
-        if repo.is_dirty():
-            repo.git.add(u=True)
-            repo.git.commit('-m', '[Backup Commit]', author='radu.v.valentin@gmail.com')
-            repo.git.push()
+        
+        
         
